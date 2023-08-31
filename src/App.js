8eee9fe10a2651b09e6e0eb92e67edd410429ac1
Chiran Gurung <grgchiran08@yahoo.com>
2023-08-30 20:08:16 -0400

    Load questions from an API (json.server)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,58 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+
+const initialState = {
+  questions: [],
+
+  //'loading', 'error', 'ready','active','finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        question: action.payload,
+        staus: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Acion unknown");
+  }
+}
 
 function App() {
-  return <DateCounter />;
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    async function getQuestions() {
+      try {
+        const res = await fetch("http://localhost:8000/questions");
+        const data = await res.json();
+        dispatch({ type: "dataReceived", payload: data });
+      } catch (error) {
+        dispatch({ type: "dataFailed" });
+      }
+    }
+    getQuestions();
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Questions?</p>
+      </Main>
+    </div>
+  );
 }
 
 export default App;
