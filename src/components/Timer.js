838b242b0fbffe8c77f88518af7a8081dbf82dbd
Chiran Gurung <grgchiran08@yahoo.com>
2023-09-10 20:39:10 -0400

    added timer

---
+++ src/components/Timer.js
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+
+function Timer({ dispatch, secondsRemaining }) {
+  const min = Math.floor(secondsRemaining / 60);
+  const sec = secondsRemaining % 60;
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      dispatch({ type: "tick" });
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [dispatch]);
+
+  return (
+    <div className="timer">
+      {min < 10 && sec < 10
+        ? `0${min}:0${sec}`
+        : min < 10
+        ? `0${min}:${sec}`
+        : `${min}:${sec}`}
+    </div>
+  );
+}
+
+export default Timer;
